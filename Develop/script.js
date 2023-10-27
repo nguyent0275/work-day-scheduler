bf98a00210eb7b4e376a2fb5159c96542ee3125f
nguyent0275 <nguyent0275@gmail.com>
2023-10-27 00:27:50 -0400

    removed html hard code and added javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,43 @@
+// var localizedFormat = require('dayjs/plugin/localizedFormat')
+// dayjs.extend(localizedFormat)
+
+let divContainerEl = $(".container-fluid")
+let currentTime = $("#currentDay")
+let currentDate = dayjs().format('MM/DD/YYYY')
+let currentHour = dayjs().format('h')
+const startTime = dayjs().set('hour', 9)
+const endTime = dayjs().set('hour', 5)
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+currentTime.text(currentDate)
+function renderScheduler() {
+  for (let i = 0; i < 9; i++) {
+    //create
+    let hourContainer = $('<div>')
+    let hour = $('<div>')
+    let task = $('<textarea>')
+    let saveButton = $('<button>')
+    //attr/text
+    hourContainer.attr({'class': 'row time-block'})
+    hour.attr({'class': 'col-2 col-md-1 hour text-center py-3'})
+    task.attr({'class': 'col-8 col-md-10 description'})
+    saveButton.attr({'class': 'btn saveBtn col-2 col-md-1'})
+    //append
+    divContainerEl.append(hourContainer)
+    hourContainer.append(hour)
+    hourContainer.append(task)
+    hourContainer.append(saveButton)
+    setTime(hour)
+    
+  }
+};
+renderScheduler()
+
+function setTime(hour){
+  
+
+}
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +56,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
