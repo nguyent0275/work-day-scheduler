e86fe9067f962551af5e3808b3b4571d73b43560
nguyent0275 <nguyent0275@gmail.com>
2023-10-27 22:09:39 -0400

    added js and code comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,89 @@
-// var localizedFormat = require('dayjs/plugin/localizedFormat')
-// dayjs.extend(localizedFormat)
-
-let divContainerEl = $(".container-fluid")
-let currentTime = $("#currentDay")
-let currentDate = dayjs().format('MM/DD/YYYY')
-let currentHour = dayjs().format('h')
-const startTime = dayjs().set('hour', 9)
-const endTime = dayjs().set('hour', 5)
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-currentTime.text(currentDate)
-function renderScheduler() {
-  for (let i = 0; i < 9; i++) {
-    //create
-    let hourContainer = $('<div>')
-    let hour = $('<div>')
-    let task = $('<textarea>')
-    let saveButton = $('<button>')
-    //attr/text
-    hourContainer.attr({'class': 'row time-block'})
-    hour.attr({'class': 'col-2 col-md-1 hour text-center py-3'})
-    task.attr({'class': 'col-8 col-md-10 description'})
-    saveButton.attr({'class': 'btn saveBtn col-2 col-md-1'})
-    //append
-    divContainerEl.append(hourContainer)
-    hourContainer.append(hour)
-    hourContainer.append(task)
-    hourContainer.append(saveButton)
-    setTime(hour)
-    
+// jquery detecting if page is ready
+$(document).ready(function () {
+  console.log("ready!");
+  // the div that contains the whole work scheduler
+  let divContainerEl = $(".container-fluid");
+  // selecting the <p> tag to display the current Date and hour
+  let currentTime = $("#currentDay");
+  // using dayjs to format the current date and hour
+  let currentDate = dayjs().format("MMMM DD YYYY hh:mm A");
+  let currentHour = dayjs().format("HH");
+  // hour and pmConverter are set as global empty strings to be used later
+  let hour = "";
+  // stores AM into a variable to be used to display hours correctly
+  let amPM = "AM";
+  let pmConverter = "";
+  // displaying the current date and hour to the page
+  currentTime.text(currentDate);
+  function renderScheduler() {
+    // setting the hour to 9, which correlates to the starting hour. hour < 18 sets the forloop to iterate 9 times up to 5pm which is the final hour.
+    for (hour = 9; hour < 18; hour++) {
+      //creates the elements for the html
+      let hourContainer = $("<div>");
+      let hourDiv = $("<div>");
+      let taskText = $("<textarea>");
+      let saveButton = $("<button>");
+      //sets classes and text to the element
+      hourContainer.attr({ class: "row time-block" });
+      hourDiv.attr({ class: "col-2 col-md-1 hour text-center py-3" });
+      taskText.attr({ class: "col-8 col-md-10 description" });
+      saveButton.attr({
+        class: "btn saveBtn col-2 col-md-1 material-symbols-outlined",
+      });
+      saveButton.text("save");
+      //append the smaller div container and 3 elements to the smaller container
+      divContainerEl.append(hourContainer);
+      hourContainer.append(hourDiv);
+      hourContainer.append(taskText);
+      hourContainer.append(saveButton);
+      // if the hour is before or equal to 12, then it will end with AM and is set to the page
+      if (hour <= 12) {
+        amPM = "AM";
+        hourDiv.text(hour + amPM);
+        // saving the hour as a value to the save button and an id to the textarea
+        saveButton.attr({ value: hour });
+        taskText.attr({ id: hour });
+      } else {
+        // if the hour is after 12 it will end with PM
+        amPM = "PM";
+      }
+      // if hour is after 12, the standard 0-24 clock will be converted to 0-12 via subtraction of 12
+      if (hour > 12) {
+        // new hour value is stored under pmConverter and is set to the page
+        pmConverter = hour - 12;
+        hourDiv.text(pmConverter + amPM);
+        // saving the hour as a value to the save button and an id to the textarea
+        saveButton.attr({ value: pmConverter });
+        taskText.attr({ id: pmConverter });
+      }
+      // if the hour is before the current time it will be designated with class "past"
+      if (hour < currentHour) {
+        hourContainer.addClass("past");
+        // if the hour is the same as the current time it will be designated with class "present"
+      } else if (hour === +currentHour) {
+        hourContainer.addClass("present");
+        // if the hour is after the current time it will be designated with class "futre"
+      } else {
+        hourContainer.addClass("future");
+      }
+    }
   }
-};
-renderScheduler()
+  renderScheduler();
+  // targeting the save button on html
+  let saveButton = $(".btn");
 
-function setTime(hour){
-  
-
-}
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+  saveButton.on("click", saveLocal);
+  function saveLocal() {
+    // the clicked button's value will be assigned to hour number (the value is equal to the hour of the container that the button is located)
+    const hourNumber = $(this).attr("value");
+    // targeting the textarea's id based on the clicked value by adding a string of '#'
+    const textAreaEl = $("#" + hourNumber);
+    // the text and hour values are stored within an object
+    const toDo = {
+      time: hourNumber,
+      todoText: textAreaEl.val(),
+    };
+    // the object is parsed into a JSON string and locally stored
+    localStorage.setItem("todo", JSON.stringify(toDo.val));
+  }
+});
